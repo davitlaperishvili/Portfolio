e5a9179c48fb97bf6423dca224df42ec99d886fc
Davit Laperishvili <>
2020-09-30 17:17:55 +0400

    landing page progress

---
+++ index.php
@@ -0,0 +1,190 @@
+<!doctype html>
+<html lang="">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>
+<link rel="shortcut icon" href="<?php // echo base_url(); ?>themes/images/favicon.png">
+<!-- <meta name="theme-color" content="#000">
+<meta name="msapplication-navbutton-color" content="#000">
+<meta name="apple-mobile-web-app-status-bar-style" content="#000"> -->
+<link rel="stylesheet" href="scripts/css/main.css">
+<script type="text/javascript" src="scripts/js/main.js"></script>
+</head>
+<body>
+
+<div class="resp_menu">
+	<div class="resp_menu_toggle">
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+	</div>
+	<div class="resp_menu_ul"></div>
+</div>
+
+<header class="trans-all-4">
+
+</header>
+
+<main class="trans-all-4">
+    <div class="container">
+        <section class="left_section">
+            <header class="left_section_header">
+                <figure>
+                    <img src="themes/images/no_image.jpg" alt="">
+                </figure>
+                <div class="left_section_header_text">
+                    <h2>Davit Laperishvili</h2>
+                    <div class="email">
+                        <a href="mailto:davitlaperishvili@gmail.com">davitlaperishvili@gmail.com</a>
+                    </div>
+                </div>
+            </header>
+            <div class="left_section_navigation">
+                <nav class="navigation">
+                    <ul>
+                        <li class="active">
+                            <a href="#">Main</a>
+                        </li>
+                        <li>
+                            <a href="#">Works</a>
+                        </li>
+                        <li>
+                            <a href="#">Other Information</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </section>
+
+        <section class="right_section trans-all-4">
+            <h2 class="right_section_title">Works</h2>
+            <div class="content">
+                <div class="works_listing">
+                    <ul>
+                        <li>
+                            <figure>
+                                <a href="#">
+                                    <img src="themes/images/no_image.jpg" alt="">
+                                </a>
+                                <div class="hover_block">
+                                    <a href="#">Visite site</a>
+                                </div>
+                            </figure>
+                            <div class="work_details">
+                                <h3 class="work_title">
+                                    <a href="#">Work title</a>
+                                </h3>
+                                <div class="work_desc">
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt eligendi debitis blanditiis mollitia ut explicabo minima soluta, voluptates ullam earum. Obcaecati facere, neque exercitationem deleniti eos excepturi laudantium iusto facilis?
+                                </div>
+                                <div class="visite_site">
+                                    <a href="#">Visite site</a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <figure>
+                                <a href="#">
+                                    <img src="themes/images/no_image.jpg" alt="">
+                                </a>
+                                <div class="hover_block">
+                                    <a href="#">Visite site</a>
+                                </div>
+                            </figure>
+                            <div class="work_details">
+                                <h3 class="work_title">
+                                    <a href="#">Work title</a>
+                                </h3>
+                                <div class="work_desc">
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt eligendi debitis blanditiis mollitia ut explicabo minima soluta, voluptates ullam earum. Obcaecati facere, neque exercitationem deleniti eos excepturi laudantium iusto facilis?
+                                </div>
+                                <div class="visite_site">
+                                    <a href="#">Visite site</a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <figure>
+                                <a href="#">
+                                    <img src="themes/images/no_image.jpg" alt="">
+                                </a>
+                                <div class="hover_block">
+                                    <a href="#">Visite site</a>
+                                </div>
+                            </figure>
+                            <div class="work_details">
+                                <h3 class="work_title">
+                                    <a href="#">Work title</a>
+                                </h3>
+                                <div class="work_desc">
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt eligendi debitis blanditiis mollitia ut explicabo minima soluta, voluptates ullam earum. Obcaecati facere, neque exercitationem deleniti eos excepturi laudantium iusto facilis?
+                                </div>
+                                <div class="visite_site">
+                                    <a href="#">Visite site</a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <figure>
+                                <a href="#">
+                                    <img src="themes/images/no_image.jpg" alt="">
+                                </a>
+                                <div class="hover_block">
+                                    <a href="#">Visite site</a>
+                                </div>
+                            </figure>
+                            <div class="work_details">
+                                <h3 class="work_title">
+                                    <a href="#">Work title</a>
+                                </h3>
+                                <div class="work_desc">
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt eligendi debitis blanditiis mollitia ut explicabo minima soluta, voluptates ullam earum. Obcaecati facere, neque exercitationem deleniti eos excepturi laudantium iusto facilis?
+                                </div>
+                                <div class="visite_site">
+                                    <a href="#">Visite site</a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <figure>
+                                <a href="#">
+                                    <img src="themes/images/no_image.jpg" alt="">
+                                </a>
+                                <div class="hover_block">
+                                    <a href="#">Visite site</a>
+                                </div>
+                            </figure>
+                            <div class="work_details">
+                                <h3 class="work_title">
+                                    <a href="#">Work title</a>
+                                </h3>
+                                <div class="work_desc">
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt eligendi debitis blanditiis mollitia ut explicabo minima soluta, voluptates ullam earum. Obcaecati facere, neque exercitationem deleniti eos excepturi laudantium iusto facilis?
+                                </div>
+                                <div class="visite_site">
+                                    <a href="#">Visite site</a>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </div>
+</main>
+
+
+
+
+
+<footer class="trans-all-4">
+
+</footer>
+
+</body>
+</html>
