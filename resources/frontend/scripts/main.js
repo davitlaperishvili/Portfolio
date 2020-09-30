8af489e5cde0083a42e05650eb36a5f94b7b05f6
Davit Laperishvili <>
2020-09-30 17:57:46 +0400

    landing page progress / tabs added

--- resources/frontend/scripts/main.js
+++ resources/frontend/scripts/main.js
@@ -13,5 +13,29 @@ window.jQuery = $;
 window.$ = $;
 
 $(document).ready(function(){
-	// ...
+	$('nav.navigation ul li a').each( function(){
+		$(this).on('click', function(){
+			const dataValue = $(this).attr('data-num');
+			const title = $(this).text();
+			//add active class to Menu Item
+			$('nav.navigation ul li').each( function(){
+				$(this).removeClass('active');
+			})
+			$(this).parent().addClass('active');
+
+			// remove all active content
+			$('div.content').each( function(){
+				$(this).removeClass('active');
+			})
+
+			// dipslay choosed content
+			const activeContent = $(`div.content[data-num=content${dataValue}]`);
+			activeContent.addClass('active');
+			
+			// Change title 
+			$('h2.right_section_title').text(title)
+		});
+	});
+	console.log($('nav.navigation ul li a'))
+
 });
